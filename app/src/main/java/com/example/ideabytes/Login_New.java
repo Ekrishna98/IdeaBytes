e7da49b425cc88b6591df797f66aaa61423cfbf2
EKrishna98 <eldhandi.krishna98@gmail.com>
2022-07-12 11:54:46 +0530

    Edit New DesignCode

---
+++ app/src/main/java/com/example/ideabytes/Login_New.java
@@ -0,0 +1,14 @@
+package com.example.ideabytes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_New extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_new);
+    }
+}
